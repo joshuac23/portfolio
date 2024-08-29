b7dc424e52c784fb3692c96fa6658d2a87063543
joshua <josh_jay23@hotmail.com>
2024-08-29 12:20:54 -0700

    modified about me section. Added self description and skills section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,23 @@
 function about() {
     return (
         <>
-        <div className="min-h-screen">
-            <h2>About Me</h2>
-            <div>Some things about me. Bachelor of Computer Science student at UBC</div>
+        <div className="min-h-screen flex justify-evenly">
+            <div>
+                <h2>About Me</h2>
+                <p>4th Year Bachelor of Computer Science student at UBC.</p>
+                <p>Graduated in 2021 Bachelor of Microbiology and Immunology at UBC</p>
+            </div>
+
+            <div>
+                <h3>Skills</h3>
+                <ul>
+                    <li>HTML/CSS</li>
+                    <li>React</li>
+                    <li>Java</li>
+                    <li>Javascript</li>
+                    <li>Python</li>
+                </ul>
+            </div>
         </div>
         </>
     )
