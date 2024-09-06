9ce4ab830ca64b96b87d3e9c78ee6f01a2f51fd6
joshua <josh_jay23@hotmail.com>
2024-09-05 17:48:35 -0700

    created a project component for projects section. Added sample projects with generic description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,14 @@
+import Project from "./Project";
+
 function projects() {
     return (
         <>
         <div className="min-h-screen">
-            <h2>Projects</h2>
-            <ul>
-                <li>Django Project</li>
-                <li>Meal Tracker</li>
-                <li>Inventory tracker?</li>
-                <li>Craigslist Redesign</li>
-            </ul>
+            <h2 className="">Projects</h2>
+            <Project className="p-4" title="Django Project" description="Example description about this project"/>
+            <Project className="p-4" title="Meal Tracker" description="Example description about this project"/>
+            <Project className="p-4" title="Inventory tracker?" description="Example description about this project"/>
+            <Project className="p-4" title="Craigslist Redesign" description="Example description about this project"/>
         </div>
         </>
     );
